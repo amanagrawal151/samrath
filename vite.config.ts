8f2d5d68280d72163b3c452eac2d6604843ff0cb
aman agrawal <amanagrawal150103@gmail.com>
2024-06-02 21:24:40 +0530

    trying to  deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // Change the host to localhost
+    host: 'https://samrath.onrender.com',
+  },
   base: "/will-you-be-my-valentine",
   plugins: [react()],
 });
